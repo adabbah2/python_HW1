fb0d5e77583f153fee41a78b13c4836398d13ad7
adabbah2 <ahmad_Dabbah16@hotmail.com>
2025-11-19 19:20:43 +0200

    Create homwork1.py

---
+++ homwork1.py
@@ -0,0 +1,19 @@
+question = input("Enter question number:\n")
+
+if question == '1':
+    # Q1 – Collatz
+    # כאן נכתוב את הקוד של שאלה 1
+    pass
+
+elif question == '2':
+    # Q2 – הצפנה עם השתקפות והזזה
+    # כאן נכתוב את הקוד של שאלה 2
+    pass
+
+elif question == '3':
+    # Q3 – הפלינדרום הארוך ביותר
+    # כאן נכתוב את הקוד של שאלה 3
+    pass
+
+else:
+    print("Error")
